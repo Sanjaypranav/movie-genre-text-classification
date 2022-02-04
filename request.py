7be7009612c1314b0b3e48d0e2e7ba186b5fed66
Sanjaypranav <sanjaypriya195@gmail.com>
2022-02-04 17:49:24 +0530

    initial commit 4

---
+++ request.py
@@ -0,0 +1,6 @@
+import requests
+
+# url = 'http://127.0.0.1:5000/predict'
+# requests.post(url, text=
+#     "Spider-Man is a superhero appearing in American comic books published by Marvel Comics. Created by writer-editor Stan Lee and artist Steve Ditko, he first appeared in the anthology comic book Amazing Fantasy #15 (August 1962) in the Silver Age of Comic Books. He has since been featured in movies, television shows, video games, and plays."
+# )
